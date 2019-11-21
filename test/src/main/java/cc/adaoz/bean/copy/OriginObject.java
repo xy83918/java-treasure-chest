8fa992b8686af53b23712c624f6cfe98681ecb89
jsalbert <jsalbert@itcom888.com>
2019-11-21 14:36:13 +0800

    测试BeanCopy

---
+++ test/src/main/java/cc/adaoz/bean/copy/OriginObject.java
@@ -0,0 +1,45 @@
+package cc.adaoz.bean.copy;
+
+import lombok.*;
+
+/**
+ * @author albert on 11/21/19
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Builder
+public class OriginObject {
+
+    private String s0;
+    private String s1;
+    private String s2;
+    private String s3;
+    private String s4;
+    private String s5;
+    private String s6;
+    private String s7;
+    private String s8;
+    private String s9;
+    private String s10;
+    private String s11;
+    private String s12;
+    private String s13;
+    private String s14;
+    private String s15;
+    private String s16;
+    private String s17;
+    private String s18;
+    private String s19;
+    private String s20;
+    private String s21;
+    private String s22;
+    private String s23;
+    private String s24;
+    private String s25;
+    private String s26;
+    private String s27;
+    private String s28;
+    private String s29;
+}
---
+++ test/src/main/java/cc/adaoz/bean/copy/README.md
@@ -0,0 +1,13 @@
+
+??10_000_000?
+================cglib BeanCopier??10000000?================
+testCglibBeanCopier ??: 1240
+================springframework??10000000?================
+testSpringFramework ??: 16008
+================apache BeanUtils PropertyUtils??10000000?================
+testApacheBeanUtilsPropertyUtils ??: 163039
+================apache BeanUtils??10000000?================
+
+
+????????
+https://www.jianshu.com/p/9a136ecd3838
